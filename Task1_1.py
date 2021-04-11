a8726e102e9165a5103bf0f7758ca83c6f37f265
Klimchuk Nikita <52635737+KlimchukNikita@users.noreply.github.com>
2021-04-11 14:03:18 +0300

    Task1_1 added

---
+++ Task1_1.py
@@ -0,0 +1,21 @@
+"""
+1. Поработайте с переменными,
+создайте несколько, выведите на экран,
+запросите у пользователя несколько чисел
+и строк и сохраните в переменные,
+выведите на экран.
+"""
+
+a = 'Hello'
+b = 'World!'
+
+print(f"{a}, {b}")
+
+num1 = int(input('Please enter any number... '))
+num2 = int(input('Please enter any number... '))
+
+print(f"You entered the numbers {num1} and {num2}")
+
+word = input("Please enter any word... ")
+
+print(f"{word} - it's a great choice!")
